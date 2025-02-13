8992b4c7fc7e362e54fddc3d68806143a6119f58
Morozoff Alexandre <morozoff_alexandre@outlook.com>
2025-02-13 10:23:54 +0100

    Bubble & Navlink

--- my-portfolio/src/Components/Welcome.jsx
+++ my-portfolio/src/Components/Welcome.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Welcome = () => {
   return (
@@ -7,14 +8,22 @@ const Welcome = () => {
       <h2 className="start-page__subtitle">Développeur Front-end junior</h2>
       <div className="start-page__bulle">
         <p className="start-page__bulle--p">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-          recusandae ducimus dolor perferendis distinctio eum, assumenda
-          accusantium tempora vel similique nihil ex consequuntur maxime
-          temporibus magnam! Totam iste iure dolorem?
+          Je me présente, je suis Alexandre Morozoff et bienvenue sur mon
+          portfolio en ligne.Tout juste finissant du parcours Developpeur Web de
+          la plateforme OpenClassroom, me voilà dans la grande jungle des
+          developpeurs juniors. Au cours de mon parcours, j’ai pu développer
+          plusieurs compétences que je vous présente ici au travers de ce site
+          interractif. Parcourez les différentes Bornes d’arcades pour en savoir
+          plus sur mon parcours.Enfin une fois que vous les aurez toutes visités
+          mon CV pourra être débloqués.
+          <br />
+          Bonne Visite !
         </p>
       </div>
       <div className="start-page__btn">
-        <button className="start-page__btn--start">START</button>
+        <NavLink to={'/arcade'}>
+          <button className="start-page__btn--start">Start</button>
+        </NavLink>
       </div>
       <div className="start-page__footer">
         <img
