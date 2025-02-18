1a0ea76e9ac29d8a1ccfc341608b9063e7a418da
Alexandre Morozoff <morozoff_alexandre@outlook.com>
2025-02-18 11:05:29 +0100

    link on logo git & x

--- my-portfolio/src/Components/Logo/Logo.jsx
+++ my-portfolio/src/Components/Logo/Logo.jsx
@@ -3,17 +3,27 @@ import React from 'react'
 const Logo = () => {
   return (
     <div className="logo">
-      <img
-        className="logo__git"
-        src="/Assets/Images/logo/github.png"
-        alt="Logo github"
-      />
-      <img className="logo__x" src="/Assets/Images/logo/x.png" alt="Logo x" />
-      <img
-        className="logo__linkedin"
-        src="/Assets/Images/logo/linkedin.png"
-        alt="Logo linkedin"
-      />
+      <a className="logo__link" href="https://github.com/Milkliz0rd">
+        <img
+          className="logo__link--git"
+          src="/Assets/Images/logo/github.png"
+          alt="Logo github"
+        />
+      </a>
+      <a href="https://x.com/Milkliz0rd" className="logo__link">
+        <img
+          className="logo__link--x"
+          src="/Assets/Images/logo/x.png"
+          alt="Logo x"
+        />
+      </a>
+      <a href="#">
+        <img
+          className="logo__link--linkedin"
+          src="/Assets/Images/logo/linkedin.png"
+          alt="Logo linkedin"
+        />
+      </a>
     </div>
   )
 }
