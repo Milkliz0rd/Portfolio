3d7a7cc0876676804cfd27ac278d976374bd6294
Alexandre Morozoff <morozoff_alexandre@outlook.com>
2025-02-18 10:45:01 +0100

    tel number changed

--- my-portfolio/src/Components/Infos/Infos.jsx
+++ my-portfolio/src/Components/Infos/Infos.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 const Infos = () => {
   return (
     <div className="contact-box">
       <h2 className="contact-box__title">Email:</h2>
       <p className="contact-box__p">morozoff_alexandre@outlook.com</p>
       <h2 className="contact-box__title">téléphone:</h2>
-      <p className="contact-box__p">06.51.01.50.86</p>
+      <p className="contact-box__p">06.12.34.56.78</p>
     </div>
   )
 }
